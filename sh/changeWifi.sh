1707534067d24ea2314d34219cff68dc188ab6e1
jkutkut <jorgeregonzalez@gmail.com>
2020-08-15 18:58:40 +0200

    updated

--- sh/changeWifi.sh
+++ sh/changeWifi.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 dir="/dev/rfcomm0"
 
-while inotifywait -e modify $dir; do
+# while inotifywait -e modify $dir; do
+while true; do
   if tail -n1 $dir | grep wifi; then
     echo "Hey!! wifi found"
   fi
