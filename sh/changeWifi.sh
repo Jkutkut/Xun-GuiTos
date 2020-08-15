ae8f3f5423149f873517c6dc9a2b548cc20fb50d
jkutkut <jorgeregonzalez@gmail.com>
2020-08-15 19:34:39 +0200

    updated

--- sh/changeWifi.sh
+++ sh/changeWifi.sh
@@ -2,18 +2,24 @@
 dir="/dev/rfcomm0"
 
 echo "executing"
-while ! test -f $dir; do
-    echo "Bluetooth device not connected"
-    sleep 2
-done
+# while ! test -f $dir; do
+#     echo "Bluetooth device not connected"
+#     sleep 2
+# done
 
-echo "Bluetooth connected"
-while inotifywait -e modify $dir; do
-    if tail -n1 $dir; then
-    #   if tail -n1 $dir | grep wifi; then
-        # echo "Hey!! wifi found"
-        echo "hi"
-    fi
-done
+# echo "Bluetooth connected"
+# while inotifywait -e modify $dir; do
+#     if tail -n1 $dir; then
+#     #   if tail -n1 $dir | grep wifi; then
+#         # echo "Hey!! wifi found"
+#         echo "hi"
+#     fi
+# done
+
+mkfifo fifootje
+cat fifootje  | other_program &
+while read input ; do
+    echo "$input" >>fifootje
+done <&3
 
 echo "done"
