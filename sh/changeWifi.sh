4157190ee021a0ac8a8ed31d110405467eea7b6e
jkutkut <jorgeregonzalez@gmail.com>
2020-08-15 19:04:53 +0200

    updated

--- sh/changeWifi.sh
+++ sh/changeWifi.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 dir="/dev/rfcomm0"
 
-# while inotifywait -e modify $dir; do
-while true; do
+while inotifywait -e modify $dir; do
+# while true; do
+  sleep 2;
+  echo 
   if tail -n1 $dir | grep wifi; then
-    echo "Hey!! wifi found"
+    # echo "Hey!! wifi found"
+    kdialog --msgbox "Hey!! wifi found"
   fi
 done
