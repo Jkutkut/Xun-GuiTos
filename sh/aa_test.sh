05c383cb46ffe2ca905d09ec4a97b63bab235f17
jkutkut <jorgeregonzalez@gmail.com>
2020-08-16 17:33:13 +0200

    prototyping

--- sh/aa_test.sh
+++ sh/aa_test.sh
@@ -1,3 +1,26 @@
-#!/bin/bash
-dbus-org.bluez.service
-touch hola.txt
+#!/bin/sh
+# This sh file analyces the content recieved from bluetooth
+
+f="hola"
+# f="btData.txt"
+
+# echo "$(date)  - Bt Interpreter: start" >> /home/ubuntu/bluetooth/log.txt
+
+if ! test -e $f; then # If no file
+    touch $f; # Create file
+fi
+
+
+while true; do
+    if [ $(wc -w < $f) -gt 0 ]; then
+        file=$(sed -n '1,100p' $f)
+        for c in $file; do
+            echo $c
+        done
+        rm $f; touch $f # Clear the file
+        cat $f
+    else
+        break
+    fi
+done
+# echo "$(date)  - Bt Interpreter: end" >> /home/ubuntu/bluetooth/log.txt
