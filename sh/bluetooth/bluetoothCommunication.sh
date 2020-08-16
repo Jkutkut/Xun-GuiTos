63b94a744093dd9dc8f806314feda9a470f47cbe
jkutkut <jorgeregonzalez@gmail.com>
2020-08-16 15:03:46 +0200

    log modified

--- sh/bluetooth/bluetoothCommunication.sh
+++ sh/bluetooth/bluetoothCommunication.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This sh file stores on a file the msgs recived from bluetooth communication
-echo "$(date)" >> /home/ubuntu/bluetooth/log.txt
+echo "$(date) - Bt Comunication: start" >> /home/ubuntu/bluetooth/log.txt
 while true; do
   if test -e /dev/rfcomm0; then # If connected by bluetooth
     cat /dev/rfcomm0 >> /home/ubuntu/bluetooth/btData.txt
--- sh/bluetooth/bluetoothInterpreter.sh
+++ sh/bluetooth/bluetoothInterpreter.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This sh file analyces the content recieved to 
-echo "$(date)" >> /home/ubuntu/bluetooth/log.txt
+echo "$(date)  - Bt Comunication: start" >> /home/ubuntu/bluetooth/log.txt
 # while true; do
 #   if test -e /dev/rfcomm0; then # If connected by bluetooth
 #     cat /dev/rfcomm0 >> /home/ubuntu/bluetooth/btData.txt
