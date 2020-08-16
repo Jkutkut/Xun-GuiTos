6523cf0e5a9282770e6eee638642487338feaa7d
jkutkut <jorgeregonzalez@gmail.com>
2020-08-16 19:32:08 +0200

    this may work

--- sh/bluetooth/bluetoothInterpreter.sh
+++ sh/bluetooth/bluetoothInterpreter.sh
@@ -13,7 +13,16 @@ while true; do
     if [ $(wc -w < $f) -gt 0 ]; then
         file=$(sed -n '1,100p' $f)
         for c in $file; do
-            echo $c
+            # echo $c
+            type=$(echo $c | cut -d ':' -f 1)
+            case "$type" in
+                "wifi")
+                    ssid=$(echo $c | cut -d ':' -f 2)
+                    pass=$(echo $c | cut -d ':' -f 3)
+                    echo "ssid = $ssid; passw = $pass" > wifidata.txt
+                    echo "wifi changed :D" > /dev/rfcomm0
+                ;;
+            esac
         done
         rm $f 
         touch $f # Clear the file
