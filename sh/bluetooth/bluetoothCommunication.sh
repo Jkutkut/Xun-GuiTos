6b37cc6ef7756f39e276e1172be66dc0aed41216
jkutkut <jorgeregonzalez@gmail.com>
2020-08-16 21:01:47 +0200

    fixing bugs

--- sh/bluetooth/bluetoothCommunication.sh
+++ sh/bluetooth/bluetoothCommunication.sh
@@ -4,16 +4,15 @@ dir="/home/ubuntu/bluetooth/"
 # Clear the previous files
 rm -f "$dir"log.txt
 rm -f "$dir"btData.txt
-touch "$dir"log.txt
-touch "$dir"btData.txt
 
+touch "$dir"btData.txt
 
 echo "$(date) - Bt Comunication: start" >> "$dir"log.txt
 while true; do
   if test -e /dev/rfcomm0; then # If connected by bluetooth
-    cat /dev/rfcomm0 >> /home/ubuntu/bluetooth/btData.txt
+    cat /dev/rfcomm0 >> "$dir"btData.txt
   else
     sleep 5 # If device not connected, wait a bit
   fi
 done
-echo "$(date) - Bt Comunication: end" >> /home/ubuntu/bluetooth/log.txt
+echo "$(date) - Bt Comunication: end" >> "$dir"log.txt
