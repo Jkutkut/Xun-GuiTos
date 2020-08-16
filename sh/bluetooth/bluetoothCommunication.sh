baa56b3eadbadf8f1243a3a2d44e1011afb35b7d
jkutkut <jorgeregonzalez@gmail.com>
2020-08-16 20:45:50 +0200

    .

--- sh/bluetooth/bluetoothCommunication.sh
+++ sh/bluetooth/bluetoothCommunication.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 # This sh file stores on a file the msgs recived from bluetooth communication
 dir="/home/ubuntu/bluetooth/"
-echo "$(date) - Bt Comunication: start" >> "$dir"log.txt
-
 # Clear the previous files
 rm -f "$dir"log.txt
 rm -f "$dir"btData.txt
 touch "$dir"log.txt
 touch "$dir"btData.txt
 
+
+echo "$(date) - Bt Comunication: start" >> "$dir"log.txt
 while true; do
   if test -e /dev/rfcomm0; then # If connected by bluetooth
     cat /dev/rfcomm0 >> /home/ubuntu/bluetooth/btData.txt
