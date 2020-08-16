d03ba7a1e638a1ddf99cea89c120f1582704816e
jkutkut <jorgeregonzalez@gmail.com>
2020-08-16 13:31:34 +0200

    first attempt

---
+++ sh/bluetooth/bluetoothInterpreter.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This sh file analyces the content recieved to 
+echo "$(date)" >> /home/ubuntu/bluetooth/log.txt
+# while true; do
+#   if test -e /dev/rfcomm0; then # If connected by bluetooth
+#     cat /dev/rfcomm0 >> /home/ubuntu/bluetooth/btData.txt
+#   else
+#     sleep 5 # If device not connected, wait a bit
+#   fi
+# done
