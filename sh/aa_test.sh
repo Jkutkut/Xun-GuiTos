3b868fe266b49bb1add2c137dba1cc8c11894869
jkutkut <jorgeregonzalez@gmail.com>
2020-08-17 00:19:40 +0200

    bluetoothInterpreter algo improved

--- sh/aa_test.sh
+++ sh/aa_test.sh
@@ -1,27 +1,36 @@
 #!/bin/sh
 # This sh file analyces the content recieved from bluetooth
-
-f="hola"
+# sleep 5
+# dir="/home/ubuntu/bluetooth/"
+dir="./"
 # f="btData.txt"
+f="hola"
 
-# echo "$(date)  - Bt Interpreter: start" >> /home/ubuntu/bluetooth/log.txt
-
-if ! test -e $f; then # If no file
-    touch $f; # Create file
-fi
-
+cd $dir # go to the desired directory
+# echo "$(date)  - Bt Interpreter: start" >> log.txt
+echo "$(date)  - Bt Interpreter: start"
 
 while true; do
-    if [ $(wc -w < $f) -gt 0 ]; then
-        file=$(cat $f)
-        for c in $file; do
-            echo $c
-            y2=$(echo $c | cut -d ':' -f 1)
+    if [ $(wc -w < $f) -gt 0 ]; then # if more than one msg on btData file 
+        cp $f ."$f".tmp
+        echo "" > $f
+        for c in $(cat ."$f".tmp); do
+            # echo "$(date)  - Bt Interpreter: command detected: $c" >> log.txt
+            echo "$(date)  - Bt Interpreter: command detected: $c"
+            type=$(echo $c | cut -d ':' -f 1)
+            case "$type" in
+                "wifi")
+                    ssid=$(echo $c | cut -d ':' -f 2)
+                    pass=$(echo $c | cut -d ':' -f 3)
+                    # echo "ssid = $ssid; passw = $pass" > wifidata.txt
+                    echo "***   ssid = $ssid; passw = $pass    ***"
+                    # echo "WIFI changed :D" > /dev/rfcomm0
+                ;;
+            esac
         done
-        # rm $f; touch $f # Clear the file
+        rm ."$f".tmp
     else
-        break
+        sleep 1
     fi
-    break
 done
-# echo "$(date)  - Bt Interpreter: end" >> /home/ubuntu/bluetooth/log.txt
+echo "$(date)  - Bt Interpreter: end" >> log.txt
--- sh/bluetooth/bluetoothInterpreter.sh
+++ sh/bluetooth/bluetoothInterpreter.sh
@@ -1,29 +1,34 @@
 #!/bin/sh
 # This sh file analyces the content recieved from bluetooth
-
+# sleep 5
 dir="/home/ubuntu/bluetooth/"
+# dir="./"
 f="btData.txt"
+# f="hola"
 
 cd $dir # go to the desired directory
 echo "$(date)  - Bt Interpreter: start" >> log.txt
+# echo "$(date)  - Bt Interpreter: start"
 
 while true; do
     if [ $(wc -w < $f) -gt 0 ]; then # if more than one msg on btData file 
-        for c in $(cat $f); do
+        cp $f ."$f".tmp
+        echo "" > $f
+        for c in $(cat ."$f".tmp); do
             echo "$(date)  - Bt Interpreter: command detected: $c" >> log.txt
+            # echo "$(date)  - Bt Interpreter: command detected: $c"
             type=$(echo $c | cut -d ':' -f 1)
             case "$type" in
                 "wifi")
                     ssid=$(echo $c | cut -d ':' -f 2)
                     pass=$(echo $c | cut -d ':' -f 3)
                     echo "ssid = $ssid; passw = $pass" > wifidata.txt
+                    # echo "***   ssid = $ssid; passw = $pass    ***"
                     echo "WIFI changed :D" > /dev/rfcomm0
                 ;;
             esac
         done
-        rm $f
-        touch $f # Clear the file
-        cat $f
+        rm ."$f".tmp
     else
         sleep 1
     fi
