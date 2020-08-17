036753b48441b8af3c74b3f2740809a0f5aedbc8
jkutkut <jorgeregonzalez@gmail.com>
2020-08-17 22:45:58 +0200

    .

--- sh/aa_test.sh
+++ sh/aa_test.sh
@@ -1,17 +1,7 @@
-# ssid=wlan_pof6
-# pass=password
-# echo "$ssid"
-# echo 'network:
-#     ethernets:
-#         eth0:
-#             dhcp4: true
-#             optional:true
-#     version: 2
-#     wifis:
-#         wlan0:
-#             optional:true
-#             access-points:
-#                 "'$ssid'":
-#                     password: "'$pass'"
-#             dhcp4: true'
-echo "hola\n   caracola"
+a=true
+a=false
+if $a; then
+    echo true
+else
+    echo false
+fi
