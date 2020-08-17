89a6d90f17d7e682c3146ac33bf51faf122adfe6
jkutkut <jorgeregonzalez@gmail.com>
2020-08-17 22:46:40 +0200

    greeting added

--- sh/bluetooth/bluetoothCommunication.sh
+++ sh/bluetooth/bluetoothCommunication.sh
@@ -5,13 +5,20 @@ dir="/home/ubuntu/bluetooth/"
 rm -f "$dir"log.txt
 rm -f "$dir"btData.txt
 
+welcomed=false # If the wellcome msg has been sent
+
 touch "$dir"btData.txt
 
 echo "$(date) - Bt Comunication: start" >> "$dir"log.txt
 while true; do
   if test -e /dev/rfcomm0; then # If connected by bluetooth
+    if ! $welcomed; then
+      echo "Connected to rasp4 :D" >> /dev/rfcomm0
+      welcomed=true # No more greetings
+    fi
     cat /dev/rfcomm0 >> "$dir"btData.txt
   else
+    welcomed=false # Reset greeting
     sleep 5 # If device not connected, wait a bit
   fi
 done
