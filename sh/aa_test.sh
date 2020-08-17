328f9f5818d3b13d53a3545ea431db65214ef3b2
jkutkut <jorgeregonzalez@gmail.com>
2020-08-17 22:01:29 +0200

    .

--- sh/aa_test.sh
+++ sh/aa_test.sh
@@ -1,16 +1,17 @@
-ssid=wlan_pof6
-pass=password
-echo "$ssid"
-echo 'network:
-    ethernets:
-        eth0:
-            dhcp4: true
-            optional:true
-    version: 2
-    wifis:
-        wlan0:
-            optional:true
-            access-points:
-                "'$ssid'":
-                    password: "'$pass'"
-            dhcp4: true'
+# ssid=wlan_pof6
+# pass=password
+# echo "$ssid"
+# echo 'network:
+#     ethernets:
+#         eth0:
+#             dhcp4: true
+#             optional:true
+#     version: 2
+#     wifis:
+#         wlan0:
+#             optional:true
+#             access-points:
+#                 "'$ssid'":
+#                     password: "'$pass'"
+#             dhcp4: true'
+echo "hola\n   caracola"
