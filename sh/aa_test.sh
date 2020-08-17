8807cb60986cba2a4793db0fd82ccf8270cc52d0
jkutkut <jorgeregonzalez@gmail.com>
2020-08-17 23:35:14 +0200

    .

--- sh/aa_test.sh
+++ sh/aa_test.sh
@@ -1,6 +1,14 @@
 read res
 t=$(echo $res | cut -d ':' -f 2)
 # touch result
-echo $t
-$t > result 2>&1 &
+# echo $t
+t2=$(echo $t | sed 's/~/ /g')
+echo $t2
+
+$t2 > result 2>&1 &
+
+echo "Result"
+cat result
+rm result
+echo ""
 # $res > result
