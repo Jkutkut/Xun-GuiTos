561aaa7108a250526700b32ebd5c3adf36496171
jkutkut <jorgeregonzalez@gmail.com>
2020-08-24 17:14:14 +0200

    Img needs to be croped

--- Client/createPlayer/createPlayer.js
+++ Client/createPlayer/createPlayer.js
@@ -1,12 +1,7 @@
-// import {MDCRipple} from '@material/ripple';
+var loadFile = function(event) {
+	var image = document.getElementById('icon');
+    image.src = URL.createObjectURL(event.target.files[0]);
+    image.style.width = "500px";
+    image.style.height = "500px";
 
-// // window.onload() = function(){
-// const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
-// // }
-
-// document.addEventListener(
-//     "MDCAutoInit:End",function() {
-//       console.log("MDCAutoInit:End!!!");
-//     }
-//   );
-//   window.mdc.autoInit();
+};
