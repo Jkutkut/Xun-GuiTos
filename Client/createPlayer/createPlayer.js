07541f6d616b0efcd2afaea51aa61134b07c93bb
jkutkut <jorgeregonzalez@gmail.com>
2020-08-24 15:56:07 +0200

    this looks good

--- Client/createPlayer/createPlayer.js
+++ Client/createPlayer/createPlayer.js
@@ -1,5 +1,12 @@
-import {MDCRipple} from '@material/ripple';
+// import {MDCRipple} from '@material/ripple';
 
-// window.onload() = function(){
-const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
-// }
+// // window.onload() = function(){
+// const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
+// // }
+
+// document.addEventListener(
+//     "MDCAutoInit:End",function() {
+//       console.log("MDCAutoInit:End!!!");
+//     }
+//   );
+//   window.mdc.autoInit();
