5d51f50bc86bbc67b85bd3cc293a9a6522616a54
jkutkut <jorgeregonzalez@gmail.com>
2020-09-05 14:12:01 +0200

    Maybe this works

--- Res/php/test.php
+++ Res/php/test.php
@@ -1,3 +1,7 @@
 <?php
     $myPDO = new PDO('sqlite:/home/ubuntu/DB/mydatabase.db');
+    $result = $myPDO->query("SELECT * FROM phpDB;");
+    foreach($result as $row) {
+        print $row['id'] . "\n";
+    }
 ?>
