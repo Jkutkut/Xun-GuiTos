29ea3ad5d0dbfe2d2cb5fb15147871925f62b512
jkutkut <jorgeregonzalez@gmail.com>
2020-09-06 13:37:12 +0200

    nowgit push

--- DB-Server/php/getFromTable.php
+++ DB-Server/php/getFromTable.php
@@ -4,12 +4,7 @@
     $ele = ($_POST["ele"] != "")? $_POST["ele"] : "*";
 
     $data = $myPDO->query("SELECT " . $ele . " FROM " . $_POST["table"] . " WHERE " . $_POST["token"] . ";");
-    // echo "SELECT " . $ele . " FROM " . $_POST["table"] . " WHERE " . $_POST["token"] . ";";
-    // echo $data;
-    // print $myPDO->query($_REQUEST["command"]);
-    // $d = $myPDO->query("SELECT * FROM tablaEliminar WHERE id=6;");
-    // $d = $myPDO->query("" . $_GET["command"]);
-    // print $d;
+    
     foreach($data as $row) {
         print $row['id'] . " -> " . $row["other"] . "\n";
     }
