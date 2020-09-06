03aa2b9a8cd16a5a3e4b94c39dfc589070942e9f
jkutkut <jorgeregonzalez@gmail.com>
2020-09-06 12:33:16 +0200

    manual querry print

--- DB-Server/php/getFromTable.php
+++ DB-Server/php/getFromTable.php
@@ -6,5 +6,5 @@
     // $data = $myPDO->query("SELECT " . $ele . " FROM " . $_POST["table"] . " WHERE " . $_POST["token"] . ";");
     // echo "SELECT " . $ele . " FROM " . $_POST["table"] . " WHERE " . $_POST["token"] . ";";
     // echo $data;
-    echo $myPDO->query($_POST["command"]);
+    print $myPDO->query($_POST["command"]);
 ?>
