476b18aec16afca56005de370a17b04c8b84447b
jkutkut <jorgeregonzalez@gmail.com>
2020-09-06 13:36:00 +0200

    now?

--- DB-Server/php/getFromTable.php
+++ DB-Server/php/getFromTable.php
@@ -1,16 +1,16 @@
 <?php
     include("setup.php");
 
-    // $ele = ($_POST["ele"] != "")? $_POST["ele"] : "*";
+    $ele = ($_POST["ele"] != "")? $_POST["ele"] : "*";
 
-    // $data = $myPDO->query("SELECT " . $ele . " FROM " . $_POST["table"] . " WHERE " . $_POST["token"] . ";");
+    $data = $myPDO->query("SELECT " . $ele . " FROM " . $_POST["table"] . " WHERE " . $_POST["token"] . ";");
     // echo "SELECT " . $ele . " FROM " . $_POST["table"] . " WHERE " . $_POST["token"] . ";";
     // echo $data;
     // print $myPDO->query($_REQUEST["command"]);
-    $d = $myPDO->query("SELECT * FROM tablaEliminar WHERE id=6;");
+    // $d = $myPDO->query("SELECT * FROM tablaEliminar WHERE id=6;");
     // $d = $myPDO->query("" . $_GET["command"]);
     // print $d;
-    foreach($d as $row) {
-        print $row['id'] . "\n";
+    foreach($data as $row) {
+        print $row['id'] . " -> " . $row["other"] . "\n";
     }
 ?>
