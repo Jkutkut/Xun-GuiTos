e5afcaa8eb11105e90af10b9663a9e344408f4f7
jkutkut <jorgeregonzalez@gmail.com>
2020-09-06 12:09:44 +0200

    echo

--- DB-Server/php/getFromTable.php
+++ DB-Server/php/getFromTable.php
@@ -3,6 +3,7 @@
 
     $ele = ($_POST["ele"] != "")? $_POST["ele"] : "";
 
-    $data = $myPDO->query("SELECT " . $ele . " FROM " . $_POST["table"] . " WHERE " . $_POST["token"] . ";");
-    echo $data;
+    // $data = $myPDO->query("SELECT " . $ele . " FROM " . $_POST["table"] . " WHERE " . $_POST["token"] . ";");
+    echo "SELECT " . $ele . " FROM " . $_POST["table"] . " WHERE " . $_POST["token"] . ";";
+    // echo $data;
 ?>
