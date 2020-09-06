3977f0338f38dee393c7073f730efef5a8739c56
jkutkut <jorgeregonzalez@gmail.com>
2020-09-06 12:28:10 +0200

    manual

--- DB-Server/php/getFromTable.php
+++ DB-Server/php/getFromTable.php
@@ -1,9 +1,10 @@
 <?php
     include("setup.php");
 
-    $ele = ($_POST["ele"] != "")? $_POST["ele"] : "";
+    // $ele = ($_POST["ele"] != "")? $_POST["ele"] : "*";
 
     // $data = $myPDO->query("SELECT " . $ele . " FROM " . $_POST["table"] . " WHERE " . $_POST["token"] . ";");
-    echo "SELECT " . $ele . " FROM " . $_POST["table"] . " WHERE " . $_POST["token"] . ";";
+    // echo "SELECT " . $ele . " FROM " . $_POST["table"] . " WHERE " . $_POST["token"] . ";";
     // echo $data;
+    echo $myPDO->exec($_POST["command"]);
 ?>
