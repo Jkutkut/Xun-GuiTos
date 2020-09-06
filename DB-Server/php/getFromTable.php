5c8fe16ba8d22ea1e4a67fc1a5b9614cf5af0f8c
jkutkut <jorgeregonzalez@gmail.com>
2020-09-06 12:55:42 +0200

    request

--- DB-Server/php/getFromTable.php
+++ DB-Server/php/getFromTable.php
@@ -6,5 +6,5 @@
     // $data = $myPDO->query("SELECT " . $ele . " FROM " . $_POST["table"] . " WHERE " . $_POST["token"] . ";");
     // echo "SELECT " . $ele . " FROM " . $_POST["table"] . " WHERE " . $_POST["token"] . ";";
     // echo $data;
-    print $myPDO->query($_GET["command"]);
+    print $myPDO->query($_REQUEST["command"]);
 ?>
