271d470f8e0008c9eb37ff24e43fe1f6579dcd8c
jkutkut <jorgeregonzalez@gmail.com>
2020-09-06 14:09:38 +0200

    querry + print

--- DB-Server/php/clearTable.php
+++ DB-Server/php/clearTable.php
@@ -2,4 +2,5 @@
     include("setup.php");
     // $myPDO->exec("DELETE FROM " . $_GET['table'] . ";");
     $myPDO->query("DELETE FROM " . $_GET['table'] . ";");
+    print "DELETE FROM " . $_GET['table'] . ";";
 ?>
