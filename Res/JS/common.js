205ba7b8ce0a42bcebd6005e662ba42c45f1c24f
jkutkut <jorgeregonzalez@gmail.com>
2020-09-09 23:49:21 +0200

    code discovered moved to custom file

---
+++ Res/JS/common.js
@@ -0,0 +1,15 @@
+var queryString = new Array();
+
+
+window.onload = function(){
+    if (queryString.length == 0) {
+        if (window.location.search.split('?').length > 1) {
+            var params = window.location.search.split('?')[1].split('&');
+            for (let i = 0; i < params.length; i++) {
+                let key = params[i].split('=')[0];
+                let value = decodeURIComponent(params[i].split('=')[1]);
+                queryString[key] = value;
+            }
+        }
+    }
+}
