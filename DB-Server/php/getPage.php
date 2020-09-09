f6c5276477d090f5d961af9799c22ba99e47d35e
jkutkut <jorgeregonzalez@gmail.com>
2020-09-09 23:02:52 +0200

    This file was not useful. The code may be useful so it will be stored on the aa-code.md file

--- DB-Server/php/getPage.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $fileName = $_POST["webPage"];
-    // echo "start:\n" . $fileName . "\n end.";
-    $myfile = fopen($fileName, "r") or die("Unable to open the file: " . $fileName . "!");
-    echo fread($myfile,filesize($fileName));
-    fclose($myfile);
-?>
