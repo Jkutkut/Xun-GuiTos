50070448fa9b1189056bb90508cf5f25d6f89a6e
jkutkut <jorgeregonzalez@gmail.com>
2020-09-10 01:34:01 +0200

    maybe querry?

--- DB-Server/php/createPlayer.php
+++ DB-Server/php/createPlayer.php
@@ -1,5 +1,5 @@
 <?php
     include("setup.php");
-    $myPDO->querry('INSERT INTO Players ("p-id") VALUES("' . $_POST['name'] . '")');
+    $myPDO->query('INSERT INTO Players ("p-id") VALUES("' . $_POST['name'] . '")');
     echo 'INSERT INTO Players ("p-id") VALUES("' . $_POST['name'] . '")';
 ?>
