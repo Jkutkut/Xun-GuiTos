947bd68d80c59fa19800bc699d92cb7d4a010695
jkutkut <jorgeregonzalez@gmail.com>
2020-09-10 01:28:56 +0200

    seems like a problem with signs

--- DB-Server/php/createPlayer.php
+++ DB-Server/php/createPlayer.php
@@ -1,5 +1,5 @@
 <?php
     include("setup.php");
-    $myPDO->exec("INSERT INTO Players ('p-id') VALUES(" . $_POST['name'] . ")");
+    $myPDO->exec("INSERT INTO Players ('p-id') VALUES(\'" . $_POST['name'] . "\')");
     echo "INSERT INTO Players ('p-id') VALUES(\'" . $_POST['name'] . "\')";
 ?>
