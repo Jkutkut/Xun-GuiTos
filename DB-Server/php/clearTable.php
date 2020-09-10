7481b3190ed182b60d25fa56e600c205e9f94617
jkutkut <jorgeregonzalez@gmail.com>
2020-09-11 01:19:25 +0200

    .

--- DB-Server/php/clearTable.php
+++ DB-Server/php/clearTable.php
@@ -1,7 +1,7 @@
 <?php
     include("setup.php");
     $ta = $_POST['table'];
-    // $querry = "INSERT INTO Players (name) VALUES('$name')";
-    // $db->exec($querry) or die("Error at creating player :S");
+    $querry = "INSERT INTO Players (name) VALUES('$name')";
+    $db->exec($querry) or die("Error at creating player :S");
     echo "Table cleared: " . $ta;
 ?>
