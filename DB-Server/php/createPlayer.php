a756c331118d88d86179f3920d1829bdd5a17aa3
jkutkut <jorgeregonzalez@gmail.com>
2020-09-10 15:25:07 +0200

    Trying other method

--- DB-Server/php/createPlayer.php
+++ DB-Server/php/createPlayer.php
@@ -4,7 +4,7 @@
     // echo 'INSERT INTO Players ("name") VALUES("' . $_POST['name'] . '");' . "\n";
     // echo $_POST["name"] . " player created";
 
-    $sql = 'INSERT INTO projects(project_name) VALUES(:project_name)';
+    $sql = 'INSERT INTO Players ("name") VALUES("' . $_POST['name'] . '");';
     $stmt = $myPDO->pdo->prepare($sql);
     // $stmt = $this->pdo->prepare($sql);
     $stmt->bindValue(':project_name', $projectName);
