ddf903975442822e4bd32305fd2119bdac52bb60
jkutkut <jorgeregonzalez@gmail.com>
2020-09-10 19:00:08 +0200

    .

--- DB-Server/php/createPlayer.php
+++ DB-Server/php/createPlayer.php
@@ -3,8 +3,11 @@
     $name = $_POST['name'];
     $querry = "INSERT INTO Players (name) VALUES('$name')";
     echo $querry;
+    
+    echo $db->query('SELECT * FROM Players');
+
     $db->exec($querry) or die("Error at creating player :S");
     echo $_POST["name"] . " player created\n";
 
-    echo $db->query('SELECT * FROM Players');
+    
 ?>
