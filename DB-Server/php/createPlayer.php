36e6848cf94b17aaada2e8af7c64739aae549552
jkutkut <jorgeregonzalez@gmail.com>
2020-09-10 16:40:01 +0200

    It didn't work :D

--- DB-Server/php/createPlayer.php
+++ DB-Server/php/createPlayer.php
@@ -1,12 +1,12 @@
 <?php
     include("setup.php");
-    // $myPDO->query('INSERT INTO Players ("name") VALUES("' . $_POST['name'] . '");');
-    // echo 'INSERT INTO Players ("name") VALUES("' . $_POST['name'] . '");' . "\n";
-    // echo $_POST["name"] . " player created";
+    $myPDO->query('INSERT INTO Players ("name") VALUES("' . $_POST['name'] . '");');
+    echo 'INSERT INTO Players ("name") VALUES("' . $_POST['name'] . '");' . "\n";
+    echo $_POST["name"] . " player created";
 
-    $sql = 'INSERT INTO Players ("name") VALUES("' . $_POST['name'] . '");';
-    $stmt = $myPDO->prepare($sql);
-    // $stmt = $this->pdo->prepare($sql);
-    $stmt->bindValue('name', $_POST['name']);
-    $stmt->execute();
+    // $sql = 'INSERT INTO Players ("name") VALUES("' . $_POST['name'] . '");';
+    // $stmt = $myPDO->prepare($sql);
+    // // // $stmt = $this->pdo->prepare($sql);
+    // // $stmt->bindValue('name', $_POST['name']);
+    // $stmt->execute();
 ?>
