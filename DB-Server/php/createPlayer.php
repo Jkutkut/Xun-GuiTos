49ee760ad3cded4ae9150471f11553984a4efdf3
jkutkut <jorgeregonzalez@gmail.com>
2020-09-10 16:54:41 +0200

    .

--- DB-Server/php/createPlayer.php
+++ DB-Server/php/createPlayer.php
@@ -1,13 +1,7 @@
 <?php
     include("setup.php");
-    $db->exec('INSERT INTO Players (name) VALUES("' . $_POST['name'] . '");');
-    // $db->query('INSERT INTO Players (name) VALUES("adri");');
-    echo 'INSERT INTO Players ("name") VALUES("' . $_POST['name'] . '");' . "\n";
-    echo $_POST["name"] . " player created";
+    echo 'INSERT INTO Players (name) VALUES("' . $_POST['name'] . '");' . "\n";
 
-    // $sql = 'INSERT INTO Players ("name") VALUES("' . $_POST['name'] . '");';
-    // $stmt = $myPDO->prepare($sql);
-    // // // $stmt = $this->pdo->prepare($sql);
-    // // $stmt->bindValue('name', $_POST['name']);
-    // $stmt->execute();
+    $db->exec('INSERT INTO Players (name) VALUES("' . $_POST['name'] . '");');
+    echo $_POST["name"] . " player created";
 ?>
