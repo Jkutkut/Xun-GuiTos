9e594aaf9001b1642d35c85917b86d1e1c5a7f7a
jkutkut <jorgeregonzalez@gmail.com>
2020-09-10 19:35:13 +0200

    WORKSgit commit .info/DB-logic.uxf -m updatedgit commit .info/DB-logic.uxf -m updated

--- DB-Server/php/createPlayer.php
+++ DB-Server/php/createPlayer.php
@@ -2,19 +2,6 @@
     include("setup.php");
     $name = $_POST['name'];
     $querry = "INSERT INTO Players (name) VALUES('$name')";
-    echo $querry;
-    
-    // $result = $db->query('SELECT * FROM Players');
-    // foreach($result as $row) {
-    //     print $row['name'] . "\n";
-    // }
-    // $db->exec('BEGIN');
     $db->exec($querry) or die("Error at creating player :S");
-    // $db->exec('COMMIT');
-    
-    
-    
-    echo $_POST["name"] . " created\n";
-
-    
+    echo "Player created: " . $name;
 ?>
