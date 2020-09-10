3cbaae82adef995ec466cbb215caf5d6cdd642ce
jkutkut <jorgeregonzalez@gmail.com>
2020-09-10 18:08:38 +0200

    using vars and diferent commas

--- DB-Server/php/createPlayer.php
+++ DB-Server/php/createPlayer.php
@@ -1,7 +1,9 @@
 <?php
     include("setup.php");
-    echo 'INSERT INTO Players (name) VALUES("' . $_POST['name'] . '");' . "\n";
-
-    // $db->exec('INSERT INTO Players (name) VALUES("' . $_POST['name'] . '");');
+    // echo 'INSERT INTO Players (name) VALUES("' . $_POST['name'] . '");' . "\n";
+    
+    $querry = 'INSERT INTO Players (name) VALUES(\'' . $_POST['name'] . '\');' . "\n";
+    echo $querry;
+    $db->exec($querry);
     echo $_POST["name"] . " player created";
 ?>
