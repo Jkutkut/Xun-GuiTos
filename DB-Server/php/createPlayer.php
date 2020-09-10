fada66dca362d4ddbc33ca3986660339cd87340d
jkutkut <jorgeregonzalez@gmail.com>
2020-09-10 16:55:14 +0200

    .

--- DB-Server/php/createPlayer.php
+++ DB-Server/php/createPlayer.php
@@ -2,6 +2,6 @@
     include("setup.php");
     echo 'INSERT INTO Players (name) VALUES("' . $_POST['name'] . '");' . "\n";
 
-    $db->exec('INSERT INTO Players (name) VALUES("' . $_POST['name'] . '");');
+    // $db->exec('INSERT INTO Players (name) VALUES("' . $_POST['name'] . '");');
     echo $_POST["name"] . " player created";
 ?>
