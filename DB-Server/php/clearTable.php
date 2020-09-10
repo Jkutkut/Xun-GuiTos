10b8360bbdbba794e244d39f01d0a8a400b76d8a
jkutkut <jorgeregonzalez@gmail.com>
2020-09-11 00:27:46 +0200

    .

--- DB-Server/php/clearTable.php
+++ DB-Server/php/clearTable.php
@@ -3,3 +3,10 @@
     echo "DELETE FROM $_POST['table'];"
     // $db->exec("DELETE FROM $_POST['table'];");
 ?>
+<?php
+    include("setup.php");
+    $table = $_POST['name'];
+    $querry = "DELETE FROM $_POST['table'];";
+    $db->exec($querry) or die("Error at clearing the table :S");
+    echo $name . " cleared";
+?>
