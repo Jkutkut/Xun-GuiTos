09ffef47a70c26f72535f245b1bd8635104fa38b
jkutkut <jorgeregonzalez@gmail.com>
2020-09-10 18:54:24 +0200

    's

--- DB-Server/php/createPlayer.php
+++ DB-Server/php/createPlayer.php
@@ -1,6 +1,7 @@
 <?php
-    include("setup.php");    
-    $querry = "INSERT INTO Players (name) VALUES('$_POST['name']');";
+    include("setup.php");
+    $name = $_POST['name'];
+    $querry = "INSERT INTO Players (name) VALUES('$name');";
     echo $querry;
     $db->exec($querry) or die("Error at creating player :S");
     echo $_POST["name"] . " player created";
