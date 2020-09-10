b898aaa9e425d2b9a4906516216342081226466f
jkutkut <jorgeregonzalez@gmail.com>
2020-09-10 18:15:24 +0200

    line break removed

--- DB-Server/php/createPlayer.php
+++ DB-Server/php/createPlayer.php
@@ -1,8 +1,6 @@
 <?php
-    include("setup.php");
-    // echo 'INSERT INTO Players (name) VALUES("' . $_POST['name'] . '");' . "\n";
-    
-    $querry = 'INSERT INTO Players (name) VALUES(\'' . $_POST['name'] . '\');' . "\n";
+    include("setup.php");    
+    $querry = 'INSERT INTO Players (name) VALUES(\'' . $_POST['name'] . '\');';
     echo $querry;
     $db->exec($querry);
     echo $_POST["name"] . " player created";
