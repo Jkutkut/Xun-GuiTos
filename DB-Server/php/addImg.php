ff55524c2035967a80cc086cce2d022121f36654
jkutkut <jorgeregonzalez@gmail.com>
2020-09-11 22:44:27 +0200

    commented and debug code removed

--- DB-Server/php/addImg.php
+++ DB-Server/php/addImg.php
@@ -2,17 +2,15 @@
     include("setup.php");
     $user = $_POST['user']; //Name of the user
     $img = $_POST['img']; //Img of the user
-    $query = "INSERT INTO Imgs (img) VALUES('$img')";
-    $db->exec($query) or die("Error adding the img :S"); //Store the img or fail
+    $addImg = "INSERT INTO Imgs (img) VALUES('$img')";
+    $db->exec($addImg) or die("Error adding the img :S"); //Store the img or fail
 
-
+    //Get imgId once it is stored
     $getImgId = 'SELECT imgId FROM Imgs WHERE imgId = (SELECT MAX(imgId) FROM Imgs)';;
-    $imgId = intval($db->query($getImgId)) or die("Error at getting the correct imgId"); //Get imgId once stored
-  
+    $imgId = intval($db->query($getImgId)) or die("Error at getting the correct imgId");
 
-    echo $imgId . "\n";
+    //Set an id reference on the Players table
     $imgIdToPlayers = 'UPDATE Players SET imgId = ' . $imgId . ' WHERE name = \'' . $user . '\'';
-    echo $imgIdToPlayers;
     $db->exec($imgIdToPlayers) or die("Error at updating the table");
 
     echo "Img stored and linked";
