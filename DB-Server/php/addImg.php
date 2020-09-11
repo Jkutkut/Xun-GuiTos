337ec3e64a9102fe07ad59057751bfcfa51316da
jkutkut <jorgeregonzalez@gmail.com>
2020-09-11 22:14:30 +0200

    debugging 2

--- DB-Server/php/addImg.php
+++ DB-Server/php/addImg.php
@@ -6,13 +6,14 @@
     $db->exec($query) or die("Error adding the img :S"); //Store the img or fail
 
 
-    $getImgId = 'SELECT imgId FROM Imgs WHERE img = $img';
+    $getImgId = 'SELECT * FROM Imgs WHERE \'imgId\' = (SELECT MAX(\'imgId\') FROM Imgs)';;
     $imgId = intval($db->query($getImgId)) or die("Error at getting the correct imgId"); //Get imgId once stored
+  
 
     echo $imgId . "\n";
-    $imgIdToPlayers = 'UPDATE Players SET imgId = $getImgId WHERE name = $user';
+    $imgIdToPlayers = 'UPDATE Players SET imgId = ' . $getImgId . ' WHERE name = ' . $user;
     echo $imgIdToPlayers;
-    $db->exec($imgIdToPlayers) or die("Error at updating the table ");
+    $db->exec($imgIdToPlayers) or die("Error at updating the table");
 
     echo "Img stored and linked";
 ?>
