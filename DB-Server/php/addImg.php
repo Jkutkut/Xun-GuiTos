c6d8b391ebe827f71571f0fc95dd19edca470245
jkutkut <jorgeregonzalez@gmail.com>
2020-09-11 20:33:10 +0200

    updated to store the link of the img on the players table

--- DB-Server/php/addImg.php
+++ DB-Server/php/addImg.php
@@ -1,7 +1,13 @@
 <?php
     include("setup.php");
-    $img = $_POST['img'];
-    $querry = "INSERT INTO Imgs (img) VALUES('$img')";
-    $db->exec($querry) or die("Error adding the img :S");
-    echo "Img stored";
+    $user = $_POST['user']; //Name of the user
+    $img = $_POST['img']; //Img of the user
+    $query = "INSERT INTO Imgs (img) VALUES('$img')";
+    $db->exec($query) or die("Error adding the img :S"); //Store the img or fail
+
+    $imgId = $db->query("SELECT imgId FROM Imgs WHERE img = 'hola'"); //Get imgId once stored
+
+    $db->exec("UPDATE Players SET imgId = $imgId WHERE name = $user");
+
+    echo "Img stored and linked";
 ?>
