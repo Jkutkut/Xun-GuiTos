a3139b519e72f497f4aa7dc72585342b60b81f47
jkutkut <jorgeregonzalez@gmail.com>
2020-11-30 22:28:37 +0100

    updated to (if works) update the phases of the waiting room using json

--- Client/waitingRoom/waitingRoom.js
+++ Client/waitingRoom/waitingRoom.js
@@ -1,12 +1,5 @@
 var phrasesIndex = [];
-const phrases = [
-    "Buscando aliados",
-    "Luchando contra chunguitos",
-    "Practicando el baile de la resistencia",
-    "Muchiflopeando los servidores",
-    "Haciendo movidas chungas",
-    ""
-];
+var phrases;
 
 function changePhrase(){
     if (phrasesIndex.length == 0){
@@ -20,41 +13,28 @@ function changePhrase(){
     $("#waitingLabel").text(phrases[phrasesIndex[randomIndex]]);
 }
 
+function processPhrases(data) {
+    phrases = data.common; // Add the common phrases
+    
+    let phr; // Extra phrases to add to "phrases"
+    if (queryString['fistTime'] == "true") { // if first time on the waiting room
+        phr = data.firstTime;
+    }
+    else {
+        phr = data.newRound;
+    }
+
+    phrases = phrases.concat(phr); // Add the extra phrases
+    $("#waitingLabel").text(phr[0]); //Set the first string from the phr array as the text now
+    setInterval(changePhrase, 5000); //Change the phrase periodically
+}
+
 window.onload = function(){
     
     getQuerry(); //function from common.js
+
+    jQuery.getJSON("phrases.json").then(processPhrases);
     
-    let phr;
-    if(queryString['fistTime'] == "true"){
-        phr = [
-            "Esperando al resto de jugadores",
-            "Analizando al resto de jugadores",
-            "Sincronizando dispositivos",
-            "Validando datos",
-            ""
-        ];
-        
-        $("#waitingLabel").text("Esperando al resto de jugadores");
-    }
-    else {
-        phr = [
-            "Pensando quién puede ser chunguito",
-            "Dudando si el de al lado miente",
-            "Será bueno el de enfrente?",
-            "Duda de tí el de la derecha seguro",
-            "El de la izquierda puede ser chunguito",
-            ""
-        ];
-        
-    }
-    for (let i = 0; i < phr.length; i++) { //for all new phrases
-        phrases.push(phr[i]); //Add them to the phases array
-    }
-    $("#waitingLabel").text(phr[0]); //Set the first string from the phr array as the text now
-    setInterval(changePhrase, 5000); //Change the phrase periodically
-
-
-
     $(".lds-ring").css("height", $(".lds-ring").css("width"));
     $("#secretBtn").click(function(){
         console.log("works");
