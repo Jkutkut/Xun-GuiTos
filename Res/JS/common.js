ba1dcac7d33918d3ffa6de9c97104ea141dacb40
jkutkut <jorgeregonzalez@gmail.com>
2020-12-01 19:17:42 +0100

    stillIn and div2disposition functions implemented

--- Res/JS/common.js
+++ Res/JS/common.js
@@ -1,3 +1,4 @@
+// Connectivity
 var queryString = new Array();
 
 function getQuerry(){
@@ -23,8 +24,38 @@ function getBase64Image(img) {
     return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
 }
 
+// Analyzers
+/**
+ * Checks whenever an element e is (phisically) inside of a element p
+ * @param {object} e - Element to check if it is inside ({x: number, y: number, w: number, h: number})
+ * @param {object} p - Parent ({x: number, y: number, w: number, h: number})
+ * @param {*boolean} noConversion - (optional) if the conversion div-disposition (see function div2disposition) shold be calculated 
+ * @returns {boolean} The result of the analysis
+ */
+function stillIn(e, p, conversion = true) {
+    if (conversion){
+        e = div2disposition(e);
+        p = div2disposition(p);
+    }
+    return !(e.x < p.x || e.y < p.y ||
+        e.x + e.w > p.x + p.w || e.y + e.h > p.y + p.h);
+}
 
 // Conversors
+/**
+ * 
+ * @param {object} e - Query element
+ * @returns {object} {x: top}
+ */
+function div2disposition(e) {
+    let posi = e.position();
+    return {
+        x: posi.top,
+        y: posi.left,
+        w: pixel2float(e.css("with")),
+        h: pixel2float(e.css("height"))
+    }
+}
 
 /**
  * Transforms a px unit to a float.
