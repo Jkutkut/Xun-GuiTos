f277fc02f2d8506cb1f11d5be086828f7376d867
jkutkut <jorgeregonzalez@gmail.com>
2020-12-02 23:44:14 +0100

    using different invocation to access DB

--- DB-Server/php/setup.php
+++ DB-Server/php/setup.php
@@ -1,5 +1,4 @@
 <?php
     // $db = new PDO('sqlite:/home/ubuntu/DB/mydatabase.db') or die("Ops! I died :S");
     $db = new SQLite3('/home/ubuntu/DB/mydatabase.db') or die("Ops! I died :S");
-    // $myPDO = new PDO('sqlite:/home/ubuntu/DB/mydatabase.db');
 ?>
