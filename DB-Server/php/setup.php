11e518a0667d5ec8357729de10fad13eebe5a8cb
jkutkut <jorgeregonzalez@gmail.com>
2020-12-02 23:41:19 +0100

    The id is obtained as a SQLite3Result, so trying to change to other type 9.1

--- DB-Server/php/setup.php
+++ DB-Server/php/setup.php
@@ -1,4 +1,5 @@
 <?php
-    $db = new PDO('sqlite:/home/ubuntu/DB/mydatabase.db') or die("Ops! I died :S");
+    // $db = new PDO('sqlite:/home/ubuntu/DB/mydatabase.db') or die("Ops! I died :S");
+    $db = new SQLite3('/home/ubuntu/DB/mydatabase.db') or die("Ops! I died :S");
     // $myPDO = new PDO('sqlite:/home/ubuntu/DB/mydatabase.db');
 ?>
