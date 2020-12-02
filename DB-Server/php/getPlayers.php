fb6c40173114d3387f380e764c6ab21f1c7297e5
jkutkut <jorgeregonzalez@gmail.com>
2020-12-03 00:59:04 +0100

    fixed? 2

--- DB-Server/php/getPlayers.php
+++ DB-Server/php/getPlayers.php
@@ -2,7 +2,7 @@
     include("setup.php");
 
     $querry = "SELECT * FROM Players";
-    $results = $db->query($getImgId)->fetchArray() or die("Error at getting the players");
+    $results = $db->query($getImgId) or die("Error at getting the players");
 
     while ($row = $results->fetchArray()) {
         var_dump($row);
