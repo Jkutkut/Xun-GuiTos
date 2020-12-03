fe66f4afad1d9522d24816fdd619179d8c748471
jkutkut <jorgeregonzalez@gmail.com>
2020-12-03 01:06:49 +0100

    fixed but working on format 2

--- DB-Server/php/getPlayers.php
+++ DB-Server/php/getPlayers.php
@@ -4,8 +4,10 @@
     $querry = "SELECT * FROM Players";
     $results = $db->query($querry) or die("Error at getting the players");
     
+    echo "data:\n";
     while ($row = $results->fetchArray()) {
         // var_dump($row);
         echo "{\"pid\":\"" . $row["pId"] . "\", \"name\":\"" . $row["name"] . "\", \"groupPos\":\"" . $row["groupPos"] . "\", \"imgId\":\"" . $row["imgId"] . "\"}";
     }
+    echo "\nend";
 ?>
