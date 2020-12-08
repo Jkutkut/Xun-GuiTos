eee4d8d9d506e7cbaac336ea3bced579a6677e7f
jkutkut <jorgeregonzalez@gmail.com>
2020-12-09 00:15:59 +0100

    simplified

--- DB-Server/php/createPlayer.php
+++ DB-Server/php/createPlayer.php
@@ -7,9 +7,6 @@
     $getpId = "SELECT pId FROM Players WHERE name = '$name'";
     $pIdQuery = $db->query($getpId)->fetchArray(SQLITE3_ASSOC);
     $pId = $pIdQuery['pId'];
-    // $pIdQuery = $db->query($getpId);
-    // $row = $pIdQuery->fetchArray(SQLITE3_ASSOC);
-    // $pId = $row['pId'];
 
     $query = "INSERT INTO Opinion (pId) VALUES('$pId')";
     $db->exec($query) or die("Error at creating opinion for the player");
