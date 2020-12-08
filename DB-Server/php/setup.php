e91e4a70116010c7f8431650e491980c62412af9
jkutkut <jorgeregonzalez@gmail.com>
2020-12-09 00:37:13 +0100

    This should prevent error when sending multiple request at the same time

--- DB-Server/php/setup.php
+++ DB-Server/php/setup.php
@@ -1,4 +1,5 @@
 <?php
     // $db = new PDO('sqlite:/home/ubuntu/DB/mydatabase.db') or die("Ops! I died :S");
     $db = new SQLite3('/home/ubuntu/DB/mydatabase.db') or die("Ops! I died :S");
+    $db->busyTimeout(2000);// Set the max time to wait to 2s. 
 ?>
