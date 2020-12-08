2c48c7d09b8edfe723a0b4c1452a1caf63de2843
jkutkut <jorgeregonzalez@gmail.com>
2020-12-08 23:46:44 +0100

    New table created based on the DB-logic: Opinion. Working on the creation of a player based on this new table

--- DB-Server/php/createPlayer.php
+++ DB-Server/php/createPlayer.php
@@ -1,7 +1,15 @@
 <?php
     include("setup.php");
     $name = $_POST['name'];
-    $querry = "INSERT INTO Players (name) VALUES('$name')";
-    $db->exec($querry) or die("Error at creating player :S");
+    $query = "INSERT INTO Players (name) VALUES('$name')";
+    $db->exec($query) or die("Error at creating player :S");
+
+    $getpId = "SELECT pId FROM Players WHERE name = '$name'";
+    $pIdQuery = $db->query($getpId);
+    $pId = $pIdQuery->fetchArray(SQLITE3_ASSOC);
+
+    $query = "INSERT INTO Players (pId) VALUES('$pId')";
+    $db->exec($query) or die("Error at creating opinion for the player");
+
     echo "Player created: " . $name;
 ?>
