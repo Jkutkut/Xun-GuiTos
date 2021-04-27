c22bec7c2ca2168e0b5246495f52fae205595aed
jkutkut <jorgeregonzalez@gmail.com>
2021-04-28 01:06:25 +0200

    trying to fix problem with imgdata on base64

--- Res/JS/common.js
+++ Res/JS/common.js
@@ -22,7 +22,8 @@ function getBase64Image(img) {
     var ctx = canvas.getContext("2d");
     ctx.drawImage(img, 0, 0);
     var dataURL = canvas.toDataURL("image/png");
-    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+    return dataURL;
+    // return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
 }
 
 function getPlayers() {
