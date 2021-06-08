c51fef9f087891537d8242ad002144848c68178f
jkutkut <jorgeregonzalez@gmail.com>
2021-06-08 23:20:28 +0200

    basic logic of mainScreen implemented

--- Client/meetup/meetup.js
+++ Client/meetup/meetup.js
@@ -1,5 +1,11 @@
-var state = false;
+const TIME2SEE = 5000;
 
 window.onload = function() {
-    
+
+    getQuerry(); //function from common.js
+
+    $("#submitBtn").click(function() {
+        $("#main").css("display", "none");
+        $("#characterReveal").css("display", "grid");
+    });
 }
