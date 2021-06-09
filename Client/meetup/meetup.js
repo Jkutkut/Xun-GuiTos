0738de8d479e84cc5179cb38404669ed461e3fb4
jkutkut <jorgeregonzalez@gmail.com>
2021-06-09 11:54:37 +0200

    Debug code changed to ajax equivalent.

--- Client/meetup/meetup.js
+++ Client/meetup/meetup.js
@@ -1,15 +1,15 @@
 const TIME2SEE = 5000;
 
 // debug
-const debugPlayers = [
-    {"pId":1,"name":"jorge","groupPos":null,"pType":1},
-    {"pId":2,"name":"paula","groupPos":null,"pType":0},
-    {"pId":3,"name":"ana","groupPos":null,"pType":1},
-    {"pId":4,"name":"adri","groupPos":null,"pType":1},
-    {"pId":5,"name":"laura","groupPos":null,"pType":1},
-    {"pId":6,"name":"user23","groupPos":null,"pType":0},
-    {"pId":7,"name":"fklsdj","groupPos":null,"pType":0}
-]
+// const debugPlayers = [
+//     {"pId":1,"name":"jorge","groupPos":null,"pType":1},
+//     {"pId":2,"name":"paula","groupPos":null,"pType":0},
+//     {"pId":3,"name":"ana","groupPos":null,"pType":1},
+//     {"pId":4,"name":"adri","groupPos":null,"pType":1},
+//     {"pId":5,"name":"laura","groupPos":null,"pType":1},
+//     {"pId":6,"name":"user23","groupPos":null,"pType":0},
+//     {"pId":7,"name":"fklsdj","groupPos":null,"pType":0}
+// ]
 
 window.onload = function() {
 
@@ -20,14 +20,14 @@ window.onload = function() {
         $("#characterReveal").css("display", "grid");
     });
 
-    // $.ajax({
-    //     url: "players",
-    //     method: "get",
-    //     success: function(data) {
-    //         getMyCharacter(data);
-    //     }
-    // });
-    getMyCharacter(debugPlayers);
+    $.ajax({
+        url: "players",
+        method: "get",
+        success: function(data) {
+            getMyCharacter(data);
+        }
+    });
+    // getMyCharacter(debugPlayers);
 }
 
 function getMyCharacter(data) {
