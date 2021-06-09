b48116316705b6ed69fbd7cdb481c3816d189f7c
jkutkut <jorgeregonzalez@gmail.com>
2021-06-09 12:14:16 +0200

    updated innocent meetup number of chunguitos. Also now on click, a timer is activated (to change to the mainMenu)

--- Client/meetup/meetup.js
+++ Client/meetup/meetup.js
@@ -1,15 +1,15 @@
-const TIME2SEE = 5000;
+const TIME2SEE = 10000;
 
 // debug
-// const debugPlayers = [
-//     {"pId":1,"name":"jorge","groupPos":null,"pType":1},
-//     {"pId":2,"name":"paula","groupPos":null,"pType":0},
-//     {"pId":3,"name":"ana","groupPos":null,"pType":1},
-//     {"pId":4,"name":"adri","groupPos":null,"pType":1},
-//     {"pId":5,"name":"laura","groupPos":null,"pType":1},
-//     {"pId":6,"name":"user23","groupPos":null,"pType":0},
-//     {"pId":7,"name":"fklsdj","groupPos":null,"pType":0}
-// ]
+const debugPlayers = [
+    {"pId":1,"name":"jorge","groupPos":null,"pType":1},
+    {"pId":2,"name":"paula","groupPos":null,"pType":0},
+    {"pId":3,"name":"ana","groupPos":null,"pType":1},
+    {"pId":4,"name":"adri","groupPos":null,"pType":0},
+    {"pId":5,"name":"laura","groupPos":null,"pType":1},
+    {"pId":6,"name":"user23","groupPos":null,"pType":0},
+    {"pId":7,"name":"fklsdj","groupPos":null,"pType":0}
+];
 
 window.onload = function() {
 
@@ -18,16 +18,20 @@ window.onload = function() {
     $("#submitBtn").click(function() {
         $("#main").css("display", "none");
         $("#characterReveal").css("display", "grid");
+
+        setTimeout(function() {
+            console.log("Change to MainMenu")
+        }, TIME2SEE);
     });
 
-    $.ajax({
-        url: "players",
-        method: "get",
-        success: function(data) {
-            getMyCharacter(data);
-        }
-    });
-    // getMyCharacter(debugPlayers);
+    // $.ajax({
+    //     url: "players",
+    //     method: "get",
+    //     success: function(data) {
+    //         getMyCharacter(data);
+    //     }
+    // });
+    getMyCharacter(debugPlayers);
 }
 
 function getMyCharacter(data) {
@@ -56,5 +60,7 @@ function getMyCharacter(data) {
         }
         $("#chunguitosP").text(chunText);
     }
-    
+    else {
+        $("#nChunguitos").text(chunguitos.length)
+    }
 }
