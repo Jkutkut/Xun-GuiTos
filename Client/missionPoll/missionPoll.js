ef41434eb456dbfe3d32b2857b66639b2ef2b4b0
jkutkut <jorgeregonzalez@gmail.com>
2021-06-20 11:14:09 +0200

    added

---
+++ Client/missionPoll/missionPoll.js
@@ -0,0 +1,14 @@
+// debug
+// const debugPlayers = [
+//     {"pId":1,"name":"jorge","groupPos":null,"pType":1},
+//     {"pId":2,"name":"paula","groupPos":null,"pType":0},
+//     {"pId":3,"name":"ana","groupPos":null,"pType":1},
+//     {"pId":4,"name":"adri","groupPos":null,"pType":0},
+//     {"pId":5,"name":"laura","groupPos":null,"pType":1},
+//     {"pId":6,"name":"user23","groupPos":null,"pType":0},
+//     {"pId":7,"name":"fklsdj","groupPos":null,"pType":0}
+// ];
+
+window.onload = function() {
+    getQuerry(); //function from common.js
+}
