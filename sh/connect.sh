7af323c7401eff49d97fc4b06be63451ca978d3a
jkutkut <jorgeregonzalez@gmail.com>
2021-06-20 13:29:53 +0200

    updated

--- sh/connect.sh
+++ sh/connect.sh
@@ -1,12 +1,15 @@
+#!/bin/sh
+
 i=2;
 # ip = 192.168.43.107
+# ip = 192.168.68.140
 
 user="";
 baseIP="192.168.68.";
 extra="";
 while true; do
-    echo "Atempting $baseIP$i$extra";
-    ssh $user@$baseIP$i ||
+    echo "Atempting $user@$baseIP$i$extra";
+    ssh -o ConnectTimeout=10 $user@$baseIP$i &&
     # curl $baseIP$i$extra &&
     echo "found!" && break ||
     i=$(($i+1));
