be021755f07141a4edd0ed218b015a317f87f928
jkutkut <jorgeregonzalez@gmail.com>
2021-06-20 11:08:44 +0200

    minor changes

--- sh/connect.sh
+++ sh/connect.sh
@@ -1,12 +1,13 @@
 i=2;
-i=104;
 # ip = 192.168.43.107
 
+user="";
 baseIP="192.168.68.";
+extra="";
 while true; do
-    echo "Atempting $baseIP$i";
-    # ssh pi@192.168.43.$i ||
-    curl $baseIP$i"/L%20Set%20ON"  &&
+    echo "Atempting $baseIP$i$extra";
+    ssh $user@$baseIP$i ||
+    # curl $baseIP$i$extra &&
     echo "found!" && break ||
     i=$(($i+1));
 done;
