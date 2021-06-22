859b7bfdf35a4a6504b19aeb097c62d478934041
jkutkut <jorgeregonzalez@gmail.com>
2021-06-23 00:10:55 +0200

    HTML, CSS and JS code to implement in the future popUps on MainMenu

--- Client/ztest/index.js
+++ Client/ztest/index.js
@@ -1,67 +1,20 @@
-// $(function(){
-// 	$('#grid').gridstrap({
-// 		/* default options */
-// 	});
-// });
-
-// function getOrden() {
-//     let children = $("#grid").children();
-//     for (let i = 0; i < children.length -1; i++) {
-//         console.log(children[i].innerHTML);
-//     }
-// }
-
-var index = 0;
-var times = 0;
-
 window.onload = function() {
-    $(window).resize(resizeTextSize);
-    resizeTextSize();
-
-    // setInterval(function(){
-    //     const arr = [
-    //         [0, 0],
-    //         [1, 0],
-    //         [0, 1],
-    //         [2, 0],
-    //         [0, 2],
-    //         [3, 0],
-    //         [0, 3]
-    //     ]
-    //     let f = function(x, j) {
-    //         if (x == 0) {
-    //             return j;
-    //         }
-    //         else {
-    //             return x;
-    //         }
-    //     }
-    //     changeBackground(f(arr[index][0], times), f(arr[index][1], times));
-    //     index++;
-    //     if (index >= arr.length) {
-    //         index = 0;
-    //         times = (times + 1);
-    //     }
-    // }, 2000);
-    changeBackground(0, 0)
 }
 
-function resizeTextSize() {
-    let size = $("body").css("height");
-    size = Number.parseInt(size.substr(0, size.length - 2) / 50);
-    console.log(size);
-
-    let textSize = [
-        ["huge", 3.5],
-        ["bigger", 3],
-        ["big", 2.5],
-        ["normal", 2],
-        ["small", 1.5],
-        ["detail", 1]
-    ]
-    for (let e of textSize) {
-        $(":root").css("--" + e[0], e[1] * size + "px");
-
+function openPopUp(type) {
+    console.log($("#resistenciaPopUp").css("display"));
+    
+    
+    let id = "#chunguitosPopUp"; // Show chunguitos'
+    if (type == 0) {
+       id = "#resistenciaPopUp"; // Show resistencia
     }
-}
+    $(".popUp").css("display", "none"); // Hide all popUps
+    $(id).css("display", "block");
 
+    $(".popUpFrame").css("display", "flex");
+
+}
+function closePopUp() {
+    $(".popUpFrame").css("display", "none");
+}
