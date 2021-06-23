e58498605880557c1bbe881fdac05e2f7d8030d0
jkutkut <jorgeregonzalez@gmail.com>
2021-06-23 10:49:51 +0200

    openPopUp function now takes a mission as the input. Ready for deploy

--- Client/ztest/index.js
+++ Client/ztest/index.js
@@ -1,19 +1,33 @@
 window.onload = function() {
 }
+var mission = {
+    mId: 1,
+    active: false,
+    leaderId: 3,
+    vYes: 4,
+    vNo: 1,
+    mRes: 0
+};
 
-function openPopUp(type) {
+
+function openPopUp(mission) {
     console.log($("#resistenciaPopUp").css("display"));
     
-    
+    // Select correct popUp
     let id = "#chunguitosPopUp"; // Show chunguitos'
-    if (type == 0) {
+    if (mission.mRes == 0) {
        id = "#resistenciaPopUp"; // Show resistencia
     }
     $(".popUp").css("display", "none"); // Hide all popUps
-    $(id).css("display", "block");
+    $(id).css("display", "block"); // Show the important one
 
-    $(".popUpFrame").css("display", "flex");
-
+    // Update popUp's score
+    $(".smallLabel").text(
+        "Éxito: " + mission.vYes +
+        " -- Fracaso: " + mission.vNo
+    );
+    
+    $(".popUpFrame").css("display", "flex"); // Show the frame with the popUps
 }
 function closePopUp() {
     $(".popUpFrame").css("display", "none");
