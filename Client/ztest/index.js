16f5a05872d03e3a92b55f17fedba3cb13256986
jkutkut <jorgeregonzalez@gmail.com>
2021-06-23 11:10:46 +0200

    popUp code moved

--- Client/ztest/index.js
+++ Client/ztest/index.js
@@ -9,32 +9,3 @@ var mission = {
     mRes: 0
 };
 
-/**
- * Takes the input mission and shows the popUp with the result of the mission.
- * @param {obj} mission - Mission to use for the popUp
- */
-function openPopUp(mission) {
-    if (mission.active || mission.vYes === null || mission.vNo === null) {
-        throw new Error("The mission hasn't finish");
-    }
-    console.log($("#resistenciaPopUp").css("display"));
-    
-    // Select correct popUp
-    let id = "#chunguitosPopUp"; // Show chunguitos'
-    if (mission.mRes == 0) {
-       id = "#resistenciaPopUp"; // Show resistencia
-    }
-    $(".popUp").css("display", "none"); // Hide all popUps
-    $(id).css("display", "block"); // Show the important one
-
-    // Update popUp's score
-    $(".smallLabel").text(
-        "Éxito: " + mission.vYes +
-        " -- Fracaso: " + mission.vNo
-    );
-    
-    $(".popUpFrame").css("display", "flex"); // Show the frame with the popUps
-}
-function closePopUp() {
-    $(".popUpFrame").css("display", "none");
-}
