53514d6329057a494edccd9aa553461c97b4cb7d
jkutkut <jorgeregonzalez@gmail.com>
2021-06-27 19:59:24 +0200

    code cleaned and commented

--- Client/missionPoll/missionPoll.js
+++ Client/missionPoll/missionPoll.js
@@ -1,35 +1,26 @@
-var increment = 0, timeout = 0;
-
-// debug
-// const debugPlayers = [
-//     {"pId":1,"name":"jorge","groupPos":null,"pType":1},
-//     {"pId":2,"name":"paula","groupPos":null,"pType":0},
-//     {"pId":3,"name":"ana","groupPos":null,"pType":1},
-//     {"pId":4,"name":"adri","groupPos":null,"pType":0},
-//     {"pId":5,"name":"laura","groupPos":null,"pType":1},
-//     {"pId":6,"name":"user23","groupPos":null,"pType":0},
-//     {"pId":7,"name":"fklsdj","groupPos":null,"pType":0}
-// ];
+const SELECTIONTIME = 400; // Time needed to hold the selection to vote.
 
 window.onload = function() {
     getQuerry(); //function from common.js
 
     // validMission
-    $(".missionCard").on("mousedwon touchstart", function(e) {
-        $(this).addClass("selected");
+    $(".missionCard").on("mousedwon touchstart", function(e) { // When missionCard pressed
+        $(this).addClass("selected"); // Start selection animation
         setTimeout(() => {
             vote(this);
-        }, 400);
-        // console.log("touchStart");
-    }).bind("mouseup mouseleave touchend", function() {
-        $(this).removeClass("selected");
-        // console.log("released");
+        }, SELECTIONTIME); // after the time, attempt to vote
+    }).bind("mouseup mouseleave touchend", function() { // When touch ended
+        $(this).removeClass("selected"); // Stop selection animation and return to normal state
     });
 }
 
+/**
+ * Attempts to make a submission of the current mission. If the card is still selected, send the vote to the server.
+ * @param {DOM Element} vDiv - missionCard selected.
+ */
 function vote(vDiv) {
-    if (!$(vDiv).hasClass("selected")) { // If pressed div isn't still selected
-        return
+    if (!$(vDiv).hasClass("selected")) { // If pressed div no longer selected
+        return;
     }
 
     let v = 1;
