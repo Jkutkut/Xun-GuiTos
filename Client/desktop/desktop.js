fc60c22057d130d31e649a173db69a638223d5b9
jkutkut <jorgeregonzalez@gmail.com>
2021-07-17 19:55:24 +0200

    ShowPlayers now display the correct containers

--- Client/desktop/desktop.js
+++ Client/desktop/desktop.js
@@ -61,3 +61,51 @@ window.onload = function() {
     $(".gun").attr("src", "../../Res/img/guns/007-gun.png");
     $(".playerIcon").attr("src", "../../Res/img/users/user13.png");
 }
+
+/** Main functions */
+
+/**
+ * @param {number} n - Number of players
+ * @returns Generator with the id of the players Divs in counterClockwise order.
+ */
+ function *playerIterator(n) {
+    if (n < 5 || n > 10 || !Number.isInteger(n)){ //if n not on the correct range or not an int
+        throw "Error at showPlayers: The value show an int be between 5 and 10, both inclusive.";
+    }
+    
+    // bottom
+    if (n == 6 || n >= 8) yield 42;
+    if (n % 2 == 1 || n == 10) yield 43;
+    if (n == 6 || n >= 8) yield 44;
+
+    // right side
+    yield 35; // always
+    if (n > 6) yield 25;
+
+    // top
+    yield 12; // always
+    if (n == 10) yield 13;
+    yield 14; // always
+    
+    // left side
+    yield 31 // always
+    if (n > 6) yield 21;
+
+}
+
+/**
+ * Given the number of players, show the containers on the correct positions.
+ * @param {number} n - The number of players on the game.
+ * @throws error if the value is not on the range [5, 10]
+ */
+ function showPlayers(n){
+    let playersDivIte = playerIterator(n);
+    $(".playerDiv").css("display", "none");
+    while (true) {
+        let pDiv = playersDivIte.next();
+        $(`#${pDiv.value}`).css("display", "flex");
+        if (pDiv.done == true) break; // If done, end
+    }
+
+    // MISSING LOGIC
+}
